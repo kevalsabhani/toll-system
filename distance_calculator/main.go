e1c6068cd7f4e4e5798b78c5c1ef62415d467747
Keval Sabhani <kevalsabhani123@gmail.com>
2024-04-19 07:06:16 +0530

    Calculate distance

--- distance_calculator/main.go
+++ distance_calculator/main.go
@@ -2,48 +2,88 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"math"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/kevalsabhani/toll-calculator/client/obu"
 	"github.com/segmentio/kafka-go"
 )
 
 const (
-	ENV       = "DEVELOPMENT"
+	env       = "DEVELOPMENT"
 	kafkaHost = "localhost:9092"
+	groupId   = "distance-calculator"
+	topic     = "obu-data"
 )
+
+var coords = [2][2]float64{}
 
 func main() {
 	//zap logger
 	// logger := zap.Must(zap.NewProduction())
-	// if ENV == "DEVELOPMENT" {
+	// if env == "DEVELOPMENT" {
 	// 	logger = zap.Must(zap.NewDevelopment())
 	// }
 
-	topic := "obu-data"
-	partition := 0
-
-	conn, err := kafka.DialLeader(context.Background(), "tcp", kafkaHost, topic, partition)
-	if err != nil {
-		log.Fatal("failed to dial leader:", err)
+	// Set up a Kafka reader
+	config := kafka.ReaderConfig{
+		Brokers:  []string{kafkaHost},
+		GroupID:  groupId,
+		Topic:    topic,
+		MinBytes: 10e3, // 10KB
+		MaxBytes: 10e6, // 10MB
 	}
+	reader := kafka.NewReader(config)
+	defer reader.Close()
 
-	batch := conn.ReadBatch(10e3, 1e6) // fetch 10KB min, 1MB max
+	// Create a signal channel to handle graceful shutdown
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
-	b := make([]byte, 10e3) // 10KB max per message
+	// Loop to read messages
 	for {
-		n, err := batch.Read(b)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		// ReadMessage blocks until it receives a message or ctx times out
+		msg, err := reader.ReadMessage(ctx)
 		if err != nil {
+			log.Printf("Error reading message: %v\n", err)
 			break
 		}
-		fmt.Println(string(b[:n]))
-	}
 
-	if err := batch.Close(); err != nil {
-		log.Fatal("failed to close batch:", err)
+		// Process the message
+		// fmt.Printf("Received message: %s\n", msg.Value)
+		distance := distanceCal(msg.Value)
+		fmt.Printf("Distance travelled: %f\n", distance)
+		select {
+		case <-sigchan:
+			fmt.Println("Received termination signal. Closing consumer.")
+			return
+		default:
+		}
 	}
+}
 
-	if err := conn.Close(); err != nil {
-		log.Fatal("failed to close connection:", err)
+func distanceCal(obuData []byte) float64 {
+	var data obu.OBUData
+	if err := json.Unmarshal(obuData, &data); err != nil {
+		log.Println("Unmarshal error: ", err)
 	}
+	if coords[1][0] == 0 && coords[1][1] == 0 {
+		coords[1][0] = data.Lat
+		coords[1][1] = data.Long
+		return 0
+	}
+	coords[0][0] = coords[1][0]
+	coords[0][1] = coords[1][1]
+	coords[1][0] = data.Lat
+	coords[1][1] = data.Long
+	return math.Sqrt(math.Pow((coords[1][0]-coords[0][0]), 2) + math.Pow((coords[1][1]-coords[0][1]), 2))
 }
